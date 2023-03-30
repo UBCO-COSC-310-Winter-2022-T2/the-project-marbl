5eaf902f08b7475bf94406d54cca3343248334bb
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-03-30 13:53:34 -0700

    Empty Chat class from diagram

---
+++ projectroot/front_end/Chat.py
@@ -0,0 +1,20 @@
+class Chat:
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
+        self.users = []
+        self.message_history = []
+
+    def get_message_history(self):
+        pass
+
+    def add_message_to_history(self, message):
+        pass
+
+    def add_user_to_chat(self, user):
+        pass
+
+    def remove_user_from_chat(self, user):
+        pass
+
+    def notify_all_users_of_chat_update(self):
+        pass
