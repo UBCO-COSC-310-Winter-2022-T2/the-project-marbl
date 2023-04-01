e4baf59b2a44a7662552493c57c34dec87b12767
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-03-31 18:03:26 -0700

    Added mqtt client and test

---
+++ projectroot/front_end/MQTTClient.py
@@ -0,0 +1,51 @@
+import paho.mqtt.client as mqtt
+
+class MQTTClient:
+    def __init__(self, broker_url):
+        self.broker_url = broker_url
+        self.client = mqtt.Client()
+        self.client.on_message = self.on_message
+        self.client.on_disconnect = self.on_disconnect
+        self.client.on_connect = self.on_connect
+
+    def start_connection(self):
+        try:
+            self.client.connect(self.broker_url)
+            self.client.loop_start()
+            return True
+        except:
+            return False
+
+    def end_connection(self):
+        self.client.loop_stop()
+        self.client.disconnect()
+        return True
+
+    def subscribe(self, chat):
+        try:
+            self.client.subscribe(chat)
+            return True
+        except:
+            return False
+
+    def publish(self, chat, msg):
+        try:
+            self.client.publish(chat, msg)
+            return True
+        except:
+            return False
+
+    def on_message(self, client, userdata, msg):
+        pass
+
+    def on_disconnect(self, client, userdata, flags, rc):
+        pass
+
+    def on_connect(self, client, userdata, flags, rc):
+        pass
+
+    def sendMessage(self, user):
+        pass
+
+    def addUser(self, user):
+        pass
