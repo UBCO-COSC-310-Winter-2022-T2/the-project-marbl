d88f14f38ef882b45d41190770005ad66f74bee6
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-04-04 21:58:01 -0700

    Register is passing tests

--- clack_root/front_end/CommandInterface.py
+++ clack_root/front_end/CommandInterface.py
@@ -21,8 +21,8 @@ class CommandInterface:
     def setCurrentChatViewed(self, chat: str) -> None:
         pass
     
-    def Login(self, username: str, password: str) -> str:
-        response = self.SM.sign_in_with_email_and_password(username, password)
+    def Login(self, email: str, password: str) -> str:
+        response = self.SM.sign_in_with_email_and_password(email, password)
         if(type(response) == dict):
             #error
             ret = {"success": False, "errorMsg": response["error"]["message"]}
@@ -32,5 +32,13 @@ class CommandInterface:
             ret = {"success": True, "session": response}
             return ret
         
-    def createAccount(self, username: str, password: str, email: str) -> bool:
-        pass
+    def createAccount(self, email: str, password: str) -> bool:
+        response = self.SM.create_user_with_email_and_password(email, password, email)
+        if("error" in response):
+            #error
+            ret = {"success": False, "errorMsg": response["error"]["message"]}
+            return ret
+        else:
+            #success
+            ret = {"success": True}
+            return ret
