9a75b49d031e28b778fd8d5df4ce41b669f61607
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-04-04 21:48:53 -0700

    tests passing for CommandInterface login

--- clack_root/front_end/CommandInterface.py
+++ clack_root/front_end/CommandInterface.py
@@ -1,10 +1,13 @@
 
-SM = None
 class CommandInterface:
     """
     Interact with the system through here by calling the methods,
     this will interact with the internal system methods and classes
     """
+    def __init__(self):
+        from front_end.Getters import getSessionManager
+        self.SM = getSessionManager()
+        pass
     
     def getUsernameOfCurrentlyLoggedInUser(self) -> str:
         pass
@@ -19,7 +22,15 @@ class CommandInterface:
         pass
     
     def Login(self, username: str, password: str) -> str:
-        pass
+        response = self.SM.sign_in_with_email_and_password(username, password)
+        if(type(response) == dict):
+            #error
+            ret = {"success": False, "errorMsg": response["error"]["message"]}
+            return ret
+        else:
+            #success
+            ret = {"success": True, "session": response}
+            return ret
         
     def createAccount(self, username: str, password: str, email: str) -> bool:
         pass
--- clack_root/test/test_front_end/Test_CommandInterface.py
+++ clack_root/test/test_front_end/Test_CommandInterface.py
@@ -1,23 +1,22 @@
 from unittest.mock import MagicMock, patch
 from front_end.Session import Session
-from front_end.CommandInterface import CommandInterface
+from front_end.Getters import getCommandInterface
 
 def test_login():
-    # test that login returns the same instance
-    with patch('front_end.CommandInterface.SM') as mock_SM:
-        def mock_func(username, password):
-            if(username == 'valid@valid.com' and password == 'valid'):
-                return Session("idtoken", "expiry", "refreshtoken", True, "valid@valid.com")
-            else:
-                return 'invalid'
-        
-        mock_SM.sign_in_with_email_and_password = mock_func
-        cmd = CommandInterface()
-        r1 = cmd.Login('valid@valid.com', 'valid')
-        r2 = cmd.Login('invalid', 'invalid')
+    example_fail_message = {'error': {'code': 400, 'errors': [{'domain': 'global', 'message': 'EMAIL_EXISTS', 'reason': 'invalid'}], 'message': 'EMAIL_EXISTS'}}
+    def mock_func(username, password):
+        if(username == 'valid@valid.com' and password == 'valid'):
+            return Session("idtoken", "expiry", "refreshtoken", True, "valid@valid.com")
+        else:
+            return example_fail_message
+    
+    cmd = getCommandInterface()
+    cmd.SM.sign_in_with_email_and_password = mock_func
+    r1 = cmd.Login('valid@valid.com', 'valid')
+    r2 = cmd.Login('invalid', 'invalid')
 
-        assert r1 and r1["success"] == True
-        assert r1 and r1["session"] is not None
+    assert r1 and r1["success"] == True
+    assert r1 and r1["session"] is not None
 
-        assert r2 and r2["success"] == False
-        assert r2 and r2["errorMsg"] is not None
+    assert r2 and r2["success"] == False
+    assert r2 and r2["errorMsg"] is not None
