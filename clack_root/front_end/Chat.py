076dd137bcf65b941c87b96c8a235df025f07bae
Adam Fipke <adamfipke@gmail.com>
2023-04-13 21:55:54 -0700

    chat tests pass

--- clack_root/front_end/Chat.py
+++ clack_root/front_end/Chat.py
@@ -1,37 +1,40 @@
 
 
-
-
-
 class Chat:
-    def __init__(self, chat_id, chat_name = "chatroom"):
+
+    def __init__(self, chat_id, chat_name=""):
         from front_end.User import UserList
         self.chat_name = chat_name
         self.chat_id = chat_id
         self.users = UserList()
         self.message_history = []
+        
+    # get users by doing myChat.users
+    
+    def get_chat_name(self) -> str:
+        return self.chat_name
 
     def get_message_history(self) -> list:
-        return None # type: ignore
+        return self.message_history
 
     def add_message_to_history(self, message) -> None:
-        pass
+        self.message_history.append(message)
 
     def add_user_to_chat(self, user) -> None:
-        pass
+        from front_end.User import User
+        self.users.append(user)
+        self.chat_name = self.chat_name + user.get_username() + " "
+        
 
     def remove_user_from_chat(self, user) -> None:
-        pass
-
-    def notify_all_users_of_chat_update(self) -> None:
-        pass
+        self.users.remove(user)
 
 
 class ChatList(list):
     '''
     list of type Chat only
     '''
-    
+
     def __init__(self, iterable=None):
         """Override initializer which can accept iterable"""
         super(ChatList, self).__init__()
@@ -53,12 +56,18 @@ class ChatList(list):
 
     def __add__(self, item):
         if isinstance(item, Chat):
-            super(ChatList, self).__add__(item) # type: ignore
+            super(ChatList, self).__add__(item)   # type: ignore
         else:
             raise ValueError('Type Chat only')
 
     def __iadd__(self, item):
         if isinstance(item, Chat):
-            super(ChatList, self).__iadd__(item) # type: ignore
+            super(ChatList, self).__iadd__(item)  # type: ignore 
         else:
             raise ValueError('Type Chat only')
+
+    def find_chat_by_name(self, name: str) -> Chat:
+        for chat in super(ChatList, self):
+            if chat.get_chat_name() == name:
+                return chat
+        return None  # type: ignore
---
+++ clack_root/test/test_front_end/test_Chat/Test_Chat.py
@@ -0,0 +1,45 @@
+from front_end.Chat import Chat
+from front_end.User import User, UserList
+
+
+def test_message_history():
+    mychat = Chat("123")
+    mychat.add_message_to_history("my cool message")
+    mymessagehistory = mychat.get_message_history()
+    assert mymessagehistory == ["my cool message"]
+
+    mychat.add_message_to_history("my cool message2")
+    mymessagehistory = mychat.get_message_history()
+    assert mymessagehistory == ["my cool message", "my cool message2"]
+
+
+def test_add_user_to_chat():
+    #create chat
+    mychat = Chat("123")
+    
+    #add first user to chat
+    testUser = User("user1", "password", "email@email.com")
+    mychat.add_user_to_chat(testUser)
+    myuserlist = mychat.users
+    
+    # check to make sure the user was added
+    assert len(myuserlist) == 1
+    myusersstring = ""
+    for user in myuserlist:
+        myusersstring = myusersstring + user.get_username()
+    assert myusersstring == "user1"
+    
+    # add second user
+    testUser = User("user2", "password", "email@email.com")
+    mychat.add_user_to_chat(testUser)
+    myuserlist = mychat.users
+    
+    # check to make sure the user was added
+    assert len(myuserlist) == 2
+    myusersstring = ""
+    for user in myuserlist:
+        myusersstring = myusersstring + user.get_username()
+    assert myusersstring == "user1user2"
+
+    # TODO: add another test case to make sure you cannot add the same user twice
+    
